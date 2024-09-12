e742f8fe9e551825e34300e47ed434a1682c5cac
OlegSadJktu <plyuha2004@mail.ru>
2024-09-12 15:25:39 +0600

    add stack implementation

---
+++ internal/stack/stack.go
@@ -0,0 +1,16 @@
+package stack
+
+type Stack[T any] []T
+
+func New[T any]() Stack[T] {
+  return make(Stack[T], 0)
+}
+
+func (st Stack[T]) Push(v T) Stack[T] {
+  return append(st, v)
+}
+
+func (st Stack[T]) Pop() (Stack[T], T) {
+  l := len(st)
+  return st[:l-1], st[l-1]
+}
